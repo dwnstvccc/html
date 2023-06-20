037cdb406f9c4a7b38e38d0842e9e3fa56efa386
dwnstvccc <136745023+dwnstvccc@users.noreply.github.com>
2023-06-20 10:23:09 -0500

    Update libreria.java

--- libreria.java
+++ libreria.java
@@ -16,7 +16,7 @@ public class PromedioLibreria {
         scanner.nextLine(); // Limpiar el buffer
 
         for (int a = 1; a <= cantidadClientess; a++) {
-            System.out.println("\nEstudiante #" + i);
+            System.out.println("\nClientes #" + a);
             System.out.print("Ingrese el número de identificación del cliente: ");
             int numeroIdentificacion = scanner.nextInt();
             scanner.nextLine(); // Limpiar el buffer
@@ -24,18 +24,18 @@ public class PromedioLibreria {
             System.out.print("Ingrese el nombre del cliente: ");
             String nombre = scanner.nextLine();
 
-            System.out.print("Ingrese el tiempo de prestamo del cliente: ");
-            String curso = scanner.nextLine();
+            System.out.print("Ingrese el tiempo de prestamo de libros hecho al cliente: ");
+            String tiempo = scanner.nextLine();
 
-            double sumaNotas = 0;
+            double sumaCantidades = 0;
             for (int i = 1; j <= 3; i++) {
-                System.out.print("Ingrese la cantidad " + i + " del cliente: ");
-                double sumacn = scanner.nextDouble();
-                sumaCantidades += sumacn;
+                System.out.print("Ingrese la cantidad de libros prestados " + i + " al cliente: ");
+                double cantidad = scanner.nextDouble();
+                sumaCantidades += cantidad;
             }
 
             // Calcular el promedio 
-            double promedio = sumacn / 3;
+            double promedio = cantidad / 3;
 
             // Imprimir los resultados
             System.out.println("\nInformación del Cliente #" + i);
